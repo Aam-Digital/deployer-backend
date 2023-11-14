4686dc7f94aed91a45f1976b73eca44d4a4e444f
Simon <simon@aam-digital.com>
2023-11-14 19:01:39 +0100

    added further properties

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,9 +7,11 @@ export class AppController {
   @Post('deploy')
   deployApp(@Body() deploymentInfo: DeploymentInfo) {
     console.log('info', deploymentInfo);
-    const args = `${deploymentInfo.name} ${
-      deploymentInfo.backend ? 'y' : 'n'
-    } ${deploymentInfo.monitor ? 'y' : 'n'}`;
+    const args = `${deploymentInfo.name} ${deploymentInfo.email} ${
+      deploymentInfo.username
+    } ${deploymentInfo.backend ? 'y' : 'n'} ${
+      deploymentInfo.monitor ? 'y' : 'n'
+    }`;
     console.log('args', args);
     const ws = fs.createWriteStream('dist/assets/arg-pipe');
     ws.write(args);
--- src/deployment-info.dto.ts
+++ src/deployment-info.dto.ts
@@ -1,5 +1,9 @@
 export class DeploymentInfo {
   name: string;
+  username: string;
+  email: string;
   backend: boolean;
   monitor: boolean;
+  apiKey: string;
+  base: string;
 }
